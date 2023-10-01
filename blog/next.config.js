16c121ff8572a8f68299d397d043203af92759e9
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-10-01 14:34:13 +0900

    Fix CD (#3)
    정적 export를 하지 않아 정상적으로 작동하지 않던 문제를 수정합니다.

--- blog/next.config.js
+++ blog/next.config.js
@@ -2,7 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	distDir: 'build',
+	output: 'export',
 	reactStrictMode: true, swcMinify: true
 }
 
