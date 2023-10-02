1d878e243100cace65203225861ba52b8e876924
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-10-02 17:00:11 +0900

    fix style

--- blog/tailwind.config.ts
+++ blog/tailwind.config.ts
@@ -7,12 +7,44 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primaryColor: '#92A8D1',
+      gray: {
+        100: '#f7fafc',
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      typography: (theme: any) => ({
+        DEFAULT: {
+          css: {
+            'pre code': {
+              span: {
+                fontFamily: 'monaco',
+              },
+            },
+            strong: {
+              color: theme('colors.primaryColor'),
+            },
+            a: {
+              textDecorationColor: theme('colors.primaryColor'),
+              textDecorationThickness: '2px',
+              textUnderlineOffset: '2px',
+            },
+            'li::marker': {
+              color: theme('colors.primaryColor'),
+            },
+            blockquote: {
+              borderLeftColor: theme('colors.primaryColor'),
+              backgroundColor: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require('@tailwindcss/typography')],
