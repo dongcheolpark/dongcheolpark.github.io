38c6ba969a544aaf1840bbb9293949cc4f8c3ce9
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-10-02 17:59:12 +0900

    Add readingTime (#12)

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
+import './page.scss';
 import { format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
 import { getMDXComponent } from 'next-contentlayer/hooks';
-import type { MDXComponents } from 'mdx/types';
+import readingTime from 'reading-time';
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
@@ -9,17 +10,6 @@ export const generateStaticParams = async () =>
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   return { title: post?.title };
-};
-
-import { useMDXComponent } from 'next-contentlayer/hooks';
-import Link from 'next/link';
-
-// Define your custom MDX components.
-const mdxComponents: MDXComponents = {
-  // Override the default <a> element to use the next/link component.
-  a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
-  // Add a custom component.
-  MyComponent: () => <div>Hello World!</div>,
 };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
@@ -32,13 +22,48 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
 
   return (
     <article className="prose py-8 mx-auto">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(post.date), 'LLLL d, yyyy')}
-        </time>
+      <div className="headerBox mb-8 text-center">
+        <div className="flex items-center justify-center gap-1 text-xs mb-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            width="14"
+            height="14"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+            ></path>
+          </svg>
+          <time dateTime={post.date} className="text-xs text-gray-600">
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time>
+          <div className="flex items-center gap-1 text-xs">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              width="14"
+              height="14"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span>{readingTime(post.body.raw).minutes.toFixed(1)}분</span>
+          </div>
+        </div>
         <h1>{post.title}</h1>
       </div>
-      <Content components={mdxComponents} />
+      <Content />
     </article>
   );
 };
