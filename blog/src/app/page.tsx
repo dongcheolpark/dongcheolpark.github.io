3bacfb26b7f659697b4eca88b4e4c4763aca5f7d
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-12-24 19:42:24 +0900

    add description (#16)

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -30,10 +30,18 @@ export default function Home() {
 
   return (
     <div className="mx-auto max-w-xl py-8">
-      <div className="ml-4 mb-8 flex items-end gap-2">
-        <h2 className="text-2xl font-bold tracking-tight md:text-4xl">
-          All Posts
-        </h2>
+      <div className="mb-8 bb-1">
+        <h1 className="mb-4 text-2xl font-bold tracking-tight md:text-4xl">
+          🛠️donngcheolpark&#39;s blog
+        </h1>
+        <p>
+          평소에 생각하고 고민했던 내용들을 공유합니다.
+          <br />
+          최근에는 코드 리뷰, 타입 시스템에 관심을 가지고 있습니다.
+        </p>
+      </div>
+      <div className="mb-8 flex items-end gap-2">
+        <h2 className="font-bold tracking-tight md:text-3xl">All Posts</h2>
         <span className="font-bold">({posts.length})</span>
       </div>
       {posts.map((post, idx) => (
